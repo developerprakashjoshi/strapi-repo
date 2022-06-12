f55b249fa885a5c6a45cdf6326ad6d915e59edd8
Prakash <developer.prakashjoshi@gmail.com>
2022-06-12 14:38:29 +0530

    Updated entity

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -17,14 +17,4 @@ module.exports = ({ env }) => {
       },
     };
   }
-
-  return {
-    connection: {
-      client: 'sqlite',
-      connection: {
-        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-      },
-      useNullAsDefault: true,
-    }
-  }
 };
